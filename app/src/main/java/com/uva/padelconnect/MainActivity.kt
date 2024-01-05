7ef7a235d11be735014ac7478ad6e815d4dc9680
MafioP <mario.pereda.08@gmail.com>
2024-01-05 12:35:58 +0100

    Fix navegacion

--- app/src/main/java/com/uva/padelconnect/MainActivity.kt
+++ app/src/main/java/com/uva/padelconnect/MainActivity.kt
@@ -3,12 +3,19 @@ package com.uva.padelconnect
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.replace
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.firebase.FirebaseApp
 import com.uva.padelconnect.databinding.ActivityMainBinding
+import com.uva.padelconnect.view.HomeFragment
+import com.uva.padelconnect.view.MatchesFragment
+import com.uva.padelconnect.view.ProfileFragment
+import com.uva.padelconnect.view.RankingFragment
+import com.uva.padelconnect.view.TournamentFragment
 
 
 class MainActivity : AppCompatActivity() {
@@ -19,22 +26,45 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setSupportActionBar(binding.toolbar2)
         setContentView(binding.root)
+        replaceFragment(HomeFragment())
 
-        val navView: BottomNavigationView = binding.navView
+        binding.bottomNavigationView.setOnItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.matchFragment -> {
+                    //Navegar al Fragmento del partido
+                    val fragment = MatchesFragment()
+                    replaceFragment(fragment)
+                    true
+                }
+                R.id.profileFragment -> {
+                    //Navegar al perfil
+                    val fragment = ProfileFragment()
+                    replaceFragment(fragment)
+                    true
+                }
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.homeFragment
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+                R.id.tournamentFragment->{
+                    //Navegar al Fragmento del Torneo
+                    val fragment= TournamentFragment()
+                    replaceFragment(fragment)
+                    true
+                }
+                R.id.homeFragment->{
+                    //Quedarse en Home
+                    replaceFragment(HomeFragment())
+                    true
+                }
+                else -> false
+            }
+        }
 
         // Inicialización de Firebase
         FirebaseApp.initializeApp(this)
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(R.id.frame_layout,fragment).commit()
     }
 /*
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
